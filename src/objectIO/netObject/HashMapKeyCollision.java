3dfd6334b7f598b98f870852724398545e534e56
xuset <xuset1@gmail.com>
2013-11-17 20:07:38 -0600

    Forgot to add the HashMapKeyCollision RuntimeException file

---
+++ src/objectIO/netObject/HashMapKeyCollision.java
@@ -0,0 +1,9 @@
+package objectIO.netObject;
+
+public class HashMapKeyCollision extends RuntimeException {
+	private static final long serialVersionUID = 7351613865057950832L;
+
+	public HashMapKeyCollision(String message) {
+		super(message);
+	}
+}
